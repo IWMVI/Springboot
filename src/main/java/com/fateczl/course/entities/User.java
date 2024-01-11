ce07c8e1c7eaef96408843eba52e28a3ca915644
Wallace Martins <wmvwallace@gmail.com>
2024-01-11 19:52:30 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/fateczl/course/entities/User.java
+++ src/main/java/com/fateczl/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.fateczl.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,17 +24,21 @@ public class User implements Serializable {
     private Long id;
     private String name;
     private String email;
-    private String fone;
+    private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
 
-    public User(Long id, String name, String email, String fone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.fone = fone;
+        this.phone = phone;
         this.password = password;
     }
 
@@ -57,12 +66,12 @@ public class User implements Serializable {
         this.email = email;
     }
 
-    public String getFone() {
-        return fone;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setFone(String fone) {
-        this.fone = fone;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getPassword() {
@@ -71,6 +80,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/fateczl/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.fateczl.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fateczl.course.entities.Order;
+
+@Repository // Opcional.
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
