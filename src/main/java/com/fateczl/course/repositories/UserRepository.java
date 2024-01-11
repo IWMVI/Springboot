1d811b710c285c2cd99a5f802fa177c3140f0b52
Wallace Martins <wmvwallace@gmail.com>
2024-01-11 18:59:23 -0300

    Service layer, component registration

--- src/main/java/com/fateczl/course/repositories/UserRepository.java
+++ src/main/java/com/fateczl/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.fateczl.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fateczl.course.entities.User;
 
+@Repository // Opcional.
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
